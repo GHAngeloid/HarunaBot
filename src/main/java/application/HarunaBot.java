1fb2b4a3e03a36b5804c86b605f048f4d73b1bd4
GHAngeloid <kelbaum1@gmail.com>
2020-11-16 00:32:41 -0500

    update builder

--- src/main/java/application/HarunaBot.java
+++ src/main/java/application/HarunaBot.java
@@ -4,7 +4,6 @@ import javax.security.auth.login.LoginException;
 
 import configuration.AppConfig;
 import listener.*;
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -18,34 +17,21 @@ import java.io.IOException;
  */
 public class HarunaBot extends ListenerAdapter{
 
-    public static void main(String[] args) throws LoginException, IOException, IllegalArgumentException{
+    public static void main(String[] args) throws LoginException, IOException, IllegalArgumentException, InterruptedException {
 
-        // builds account type
-        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        AppConfig.initializeProperties();
+
+        JDABuilder builder = JDABuilder.createDefault(AppConfig.PROPERTIES.getProperty("TOKEN"));
 
         // setting bot characteristics
         builder.setAutoReconnect(true);
         builder.setActivity(Activity.of(ActivityType.DEFAULT, "with Taichou"));
-        //builder.setAudioEnabled(true);
 
-        // include Listener classes
-        addListeners(builder);
+        // build
+        JDA jda = builder.build();
 
-        // initialize properties
-        AppConfig.initializeProperties();
-
-        // set token to JDA and build blocking
-        JDA jda = builder.setToken(AppConfig.PROPERTIES.getProperty("TOKEN")).build();
-
-    }
-
-    /**
-     * Add Listeners to JDABuilder
-     * @param builder JDABuilder
-     */
-    private static void addListeners(JDABuilder builder) {
-
-        builder.addEventListeners(new StatusListener(),
+        // add listeners
+        jda.addEventListener(new StatusListener(),
                 new GuildMemberListener(),
                 new VoiceListener(),
                 new PMListener(),
